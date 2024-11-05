30636f259aef8e6528963acd0a2c638b41ba09e9
hassansyed1029 <syedhassan1029@gmail.com>
2024-11-05 15:14:54 -0600

    final learnings about astro islands

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://boisterous-lily-68b17a.netlify.app"
+  site: "https://boisterous-lily-68b17a.netlify.app",
+  integrations: [preact()]
 });
